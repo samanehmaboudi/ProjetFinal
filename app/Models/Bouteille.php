c3070c5dc2598f84406a58f13f70c6f89156584b
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-11-18 13:20:00 -0500

    PV-8: ajout de commentaires sur le modèle Bouteille

---
+++ app/Models/Bouteille.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bouteille extends Model
+{
+    use HasFactory;
+
+    /**
+     * Attributs remplissables en création/mise à jour.
+     */
+    protected $fillable = [
+        'cellier_id', 
+        'nom',        
+        'pays',       
+        'format',     
+        'quantite',  
+        'prix',       
+    ];
+
+    /**
+     * Ici on force le prix à être un décimal avec 2 chiffres
+     */
+    protected function casts(): array
+    {
+        return [
+            'prix' => 'decimal:2',
+        ];
+    }
+
+    /**
+     * Relation : cette bouteille appartient à un cellier.
+     */
+    public function cellier()
+    {
+        return $this->belongsTo(Cellier::class);
+    }
+}

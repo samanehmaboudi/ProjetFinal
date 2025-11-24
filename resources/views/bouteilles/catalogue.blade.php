beac69b5b8fb7a532d3baf3ff260b1f662a339d6
Tommy Bourgeois <tommyb4289@gmail.com>
2025-11-24 10:57:15 -0500

    image path fix landing page

--- resources/views/bouteilles/catalogue.blade.php
+++ resources/views/bouteilles/catalogue.blade.php
@@ -5,7 +5,23 @@
 <x-page-header title="Catalogue des bouteilles" />
 <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-6">
     @foreach ($bouteilles as $bouteille)
-        <x-bouteille-card-block :id="$bouteille->id" :nom="$bouteille->nom" :image="$bouteille->image" :prix="$bouteille->prix" />
+        @php
+            // Normaliser le chemin de l'image comme dans bouteille-card
+            $imagePath = null;
+            if ($bouteille->url_image) {
+                $imagePath = ltrim($bouteille->url_image, '/');
+                while (str_starts_with($imagePath, 'storage/')) {
+                    $imagePath = substr($imagePath, 8);
+                }
+                $imagePath = asset('storage/' . $imagePath);
+            }
+        @endphp
+        <x-bouteille-card-block 
+            :id="$bouteille->id" 
+            :nom="$bouteille->nom" 
+            :image="$imagePath" 
+            :prix="$bouteille->prix" 
+        />
     @endforeach
 </div>
 </section>
